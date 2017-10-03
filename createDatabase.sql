f4d329b9ae80be53924429501b52df71b35c450d
RGomes97 <rubensgagostinho@outlook.com>
2017-10-03 20:25:07 -0300

    upload do projeto

---
+++ createDatabase.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [dbo].[Usuario]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nome] VARCHAR(200) NOT NULL, 
+    [Endereço] VARCHAR(250) NOT NULL, 
+    [CEP] VARCHAR(9) NULL, 
+    [Bairro] VARCHAR(150) NOT NULL, 
+    [Cidade] VARCHAR(150) NOT NULL, 
+    [UF] VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE [dbo].[Agenda]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[IdUsuario] INT NOT NULL,
+    [Nome] VARCHAR(100) NOT NULL, 
+    constraint fk_usuario foreign key (IdUsuario) references [dbo].[usuario](Id)
+)
+
+CREATE TABLE [dbo].[Compromisso]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [IdAgenda] INT NOT NULL, 
+	[Nome] VARCHAR(250) NOT NULL,
+    [DataInicio] DATETIME NOT NULL, 
+    [DataFim] DATETIME NULL
+	constraint fk_agenda foreign key (IdAgenda) references [dbo].[Agenda](Id)
+)
